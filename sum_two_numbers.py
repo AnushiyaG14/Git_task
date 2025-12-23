dbc2ff93b4b8e426676e72ae77af023ad9cc8e1a
AnushiyaG14 <anushiya2014@gmail.com>
2025-12-23 20:37:15 +0530

    simple python script and .gitignore added

---
+++ sum_two_numbers.py
@@ -0,0 +1,8 @@
+def sum_two_number():
+    return a+b
+if __name__ == "__main__":
+    x = int(input("enter first number"))
+    y = int(input("enter second number"))
+    print("sum value is :", sum_two_number(x,y))
+
+
